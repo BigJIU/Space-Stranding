000535cd875779d73ac7875153054b68d81a15dc
bigJIU <1572204178@qq.com>
2022-03-30 11:23:08 +0800

    330

--- Scripts/CameraManager.cs
+++ Scripts/CameraManager.cs
@@ -7,12 +7,19 @@ public class CameraManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*this.screenRect = new Rect(0,0,Screen.width,Screen.height);
+        camera = this.palyerNode.transform.Find("Main Camera").GetComponent<Camera>();*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*if(screenRect.Contains(camera.WorldToScreenPoint(gameObject.transform.GetChild(0).position)))
+        {
+            Debug.Log("第一个cube在视野内");
+        } else
+        {
+            Debug.Log("第一个cube不在视野内");
+        }*/
     }
 }
--- Scripts/Character/MoveManager.cs
+++ Scripts/Character/MoveManager.cs
@@ -1,44 +1,141 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveManager : MonoBehaviour
 {
-    private Transform trans;
+    private Transform transAs;
     private Rigidbody2D rigi;
 
-    public float m_moveSpeed;
+    public float m_moveMaxSpeed;
+    public float m_moveAccSpeed;
     public float m_rotateSpeed;
+    public float m_rotateDelaySpeed;
+    public float m_rotateMaxDegree;
+
+    private float currentSpeed = 0;
     // Start is called before the first frame update
     void Start()
     {
         rigi = this.GetComponent<Rigidbody2D>();
-        trans = this.GetComponent<Transform>();
+        transAs = transform.GetChild(0).GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey("a"))
-        {
-            //transform.Rotate(Vector3.up * m_rotateSpeed * Time.deltaTime, Space.Self);
-            trans.Rotate(axis:Vector3.up, 0.1f);
-        }
-        if (Input.GetKey("d"))
-        {
-            trans.Rotate(axis:Vector3.down, 0.1f);
-        }
+        if (Input.GetKey("r")) transform.position = new Vector3(0,0,-10);
         if (Input.GetKey("w"))
         {
-            transform.Translate(trans.up * m_moveSpeed * Time.deltaTime);
-            //rigi.AddForce(trans.forward*m_moveSpeed);
-
+            yAxisMove(0);
         }
-        if (Input.GetKey("s"))
+        
+        if(Input.GetKey("a"))
         {
-            transform.Translate(trans.up * (-m_moveSpeed) * Time.deltaTime);
-            //rigi.AddForce(trans.forward*(-m_moveSpeed));
+            if (Input.GetKey("d"))
+            {
+                yAxisMove(0);
+            }
+            yAxisMove(1);
+        }else if (Input.GetKey("d"))
+        {
+            yAxisMove(-1);
+        }
+        else
+        {
+            recoverDirection();
+        }
+        // if (Input.GetKey("a"))
+        // {
+        //     //transform.Rotate(Vector3.up * m_rotateSpeed * Time.deltaTime, Space.Self);
+        //     trans.Rotate(axis:Vector3.up, 0.1f);
+        // }
+        // if (Input.GetKey("d"))
+        // {
+        //     trans.Rotate(axis:Vector3.down, 0.1f);
+        // }
+        // if (Input.GetKey("w"))
+        // {
+        //     transform.Translate(trans.up * m_moveSpeed * Time.deltaTime);
+        //     //rigi.AddForce(trans.forward*m_moveSpeed);
+        //
+        // }
+        // if (Input.GetKey("s"))
+        // {
+        //     transform.Translate(trans.up * (-m_moveSpeed) * Time.deltaTime);
+        //     //rigi.AddForce(trans.forward*(-m_moveSpeed));
+        // }
+    }
+    private void yAxisMove(int dir){
+        transform.Translate(spaceForward()* Time.deltaTime * currentSpeed, Space.Self);
+        if (currentSpeed < m_moveMaxSpeed)
+        {
+            currentSpeed += Time.deltaTime * m_moveAccSpeed;
+        }
+        if (dir == 0) { } //forward at now direction
+        else if (dir == 1)//turn left 
+        {
+            if (yEulerAngel() > -m_rotateMaxDegree)
+            {
+                transAs.Rotate(Vector3.up, -m_rotateSpeed * Time.deltaTime);	
+            }
+        }
+        else if(dir == -1)//turn right
+        {
+            if (yEulerAngel() < m_rotateMaxDegree)
+            {
+                transAs.Rotate(Vector3.up, m_rotateSpeed * Time.deltaTime);	
+            }
+        }
+        else
+        {
+            
         }
     }
+/// <summary>
+/// recover speed to 0, rotation to 0
+/// </summary>
+    private void recoverDirection()
+    {
+        //speed recover
+        if (currentSpeed > 0)
+        {
+            transform.Translate(spaceForward()* Time.deltaTime * currentSpeed, Space.Self);
+            currentSpeed -= m_moveAccSpeed * Time.deltaTime;
+        }
+        else
+        {
+            currentSpeed = 0;
+        }
+        //rotation recover
+        if (Math.Abs(yEulerAngel()) > m_rotateDelaySpeed * Time.deltaTime)
+        {
+            transAs.Rotate(Vector3.up, (yEulerAngel()>0?-1:1)* m_rotateDelaySpeed * Time.deltaTime);
+        }
+        else if(Math.Abs(yEulerAngel()) > 1)
+        {
+            transAs.localRotation = new Quaternion(0,0,0,0);
+        }
+
+    }
+
+    private Vector3 spaceForward()
+    {
+        // Transform(transAs.forward)
+        // Vector3 tmp = .
+        // Vector3 offset = transAs.localPosition * transform.localScale;//计算相对父节点偏差  
+        // Vector3 result = parent.position + parent.rotation * offset;//父节点的转角四元数乘以差值算出子节点在世界坐标系下相对父节点的差值  
+        return transAs.forward;
+    }
+
+    private float yEulerAngel()
+    {
+        return transAs.localRotation.eulerAngles.y > 180
+            ? transAs.localRotation.eulerAngles.y - 360
+            : transAs.localRotation.eulerAngles.y;
+    }
+    
     
 }
